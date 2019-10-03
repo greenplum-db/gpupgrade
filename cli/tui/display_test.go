f053b690f537ccf2d714ca2cd548613e60bf9727
Mark Sliva <msliva@pivotal.io>
2019-10-03 13:04:03 -0600

    wip - make tests easier to follow
    Co-authored-by: David Krieger <dkrieger@pivotal.io>

--- cli/tui/display_test.go
+++ cli/tui/display_test.go
@@ -16,79 +16,40 @@ func setupTest(t *testing.T) *GomegaWithT {
 	return g
 }
 
-func TestOutputStatus(t *testing.T) {
-	var includedValid1 = map[idl.UpgradeSteps]bool{}
-	includedValid1[idl.UpgradeSteps_START_AGENTS] = true
-
-	var includedValid2 = map[idl.UpgradeSteps]bool{}
-	includedValid2[idl.UpgradeSteps_CONFIG] = true
-
-	type args struct {
-		status *idl.UpgradeStepStatus
-	}
-	validStatus1 := &idl.UpgradeStepStatus{
+func Test_OutputStatus_WithRunningInput(t *testing.T) {
+	g := setupTest(t)
+	upgradeStepStatus := &idl.UpgradeStepStatus{
 		Step:   idl.UpgradeSteps_START_AGENTS,
 		Status: idl.StepStatus_RUNNING,
 	}
 
-	validStatus2 := &idl.UpgradeStepStatus{
+	got := OutputStatus(upgradeStepStatus)
+	g.Expect(got).To(MatchRegexp(`^starting agents[.]{3}\s*\[IN_PROGRESS\]\s*\r$`))
+}
+
+func Test_OutputStatus_WithCompleteStatus(t *testing.T) {
+	g := setupTest(t)
+	unknownUpgradeStepStatus := &idl.UpgradeStepStatus{
 		Step:   idl.UpgradeSteps_CONFIG,
 		Status: idl.StepStatus_COMPLETE,
 	}
 
-	tests := []struct {
-		name string
-		args args
-		want string
-	}{
-		{
-			"OutputStatus when using a valid input that is in progress",
-			args{validStatus1},
-			`^starting agents[.]{3}\s*\[IN_PROGRESS\]\s*\r$`,
-		},
-		{
-			"OutputStatus when using a valid input that is completed",
-			args{validStatus2},
-			`^retrieving configs[.]{3}\s*\[COMPLETE\]\s*\n$`,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			g := setupTest(t)
-
-			got := OutputStatus(tt.args.status)
-			g.Expect(got).To(MatchRegexp(tt.want))
-		})
-	}
+	got := OutputStatus(unknownUpgradeStepStatus)
+	g.Expect(got).To(MatchRegexp(`^retrieving configs[.]{3}\s*\[COMPLETE\]\s*\n$`))
 }
 
-func Test_getDisplayLine(t *testing.T) {
-	type args struct {
-		step   idl.UpgradeSteps
-		status idl.StepStatus
-	}
-	tests := []struct {
-		name string
-		args args
-		want string
-	}{
-		{"getDisplayLine when using a supported step and status",
-			args{step: idl.UpgradeSteps_START_AGENTS, status: idl.StepStatus_COMPLETE},
-			`^starting agents[.]{3}\s*\[COMPLETE\]\s*$`,
-		},
-		{"getDisplayLine when unknown step and status",
-			args{step: idl.UpgradeSteps_UNKNOWN_STEP, status: idl.StepStatus_UNKNOWN_STATUS},
-			`^unknown step value[.]{3}\s*\[UNKNOWN\]\s*$`,
-		},
-	}
+func Test_getDisplayLine_UsingKnownStepStatus(t *testing.T) {
+	g := setupTest(t)
 
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			g := setupTest(t)
+	got := getDisplayLine(idl.UpgradeSteps_START_AGENTS, idl.StepStatus_COMPLETE)
+	g.Expect(got).To(MatchRegexp(`^starting agents[.]{3}\s*\[COMPLETE\]\s*$`))
+	g.Expect(len(got)).To(Equal(80))
+}
 
-			got := getDisplayLine(tt.args.step, tt.args.status)
-			g.Expect(got).To(MatchRegexp(tt.want))
-			g.Expect(len(got)).To(Equal(80))
-		})
-	}
+func Test_getDisplayLine_UsingUnknownStepStatus(t *testing.T) {
+	g := setupTest(t)
+
+	got := getDisplayLine(idl.UpgradeSteps_UNKNOWN_STEP, idl.StepStatus_UNKNOWN_STATUS)
+	g.Expect(got).To(MatchRegexp(`^unknown step value[.]{3}\s*\[UNKNOWN\]\s*$`))
+	g.Expect(len(got)).To(Equal(80))
 }
--- utils/idl_sorting_test.go
+++ utils/idl_sorting_test.go
@@ -8,35 +8,19 @@ import (
 )
 
 var (
-	len0StepStatus = []*idl.UpgradeStepStatus{}
-	len1StepStatus = []*idl.UpgradeStepStatus{
-		{
-			Step: idl.UpgradeSteps_UNKNOWN_STEP,
-		},
+	len0StepStatus     = []*idl.UpgradeStepStatus{}
+	len1StepStatus     = []*idl.UpgradeStepStatus{{Step: idl.UpgradeSteps_UNKNOWN_STEP}}
+	len2StepStatus1lt2 = []*idl.UpgradeStepStatus{
+		{Step: idl.UpgradeSteps_UNKNOWN_STEP},
+		{Step: idl.UpgradeSteps_CONFIG},
 	}
-	len2StepStatus_1lt2 = []*idl.UpgradeStepStatus{
-		{
-			Step: idl.UpgradeSteps_UNKNOWN_STEP,
-		},
-		{
-			Step: idl.UpgradeSteps_CONFIG,
-		},
+	len2StepStatus2lt1 = []*idl.UpgradeStepStatus{
+		{Step: idl.UpgradeSteps_CONFIG},
+		{Step: idl.UpgradeSteps_UNKNOWN_STEP},
 	}
-	len2StepStatus_2lt1 = []*idl.UpgradeStepStatus{
-		{
-			Step: idl.UpgradeSteps_CONFIG,
-		},
-		{
-			Step: idl.UpgradeSteps_UNKNOWN_STEP,
-		},
-	}
-	len2StepStatus_oob = []*idl.UpgradeStepStatus{
-		{
-			Step: idl.UpgradeSteps_CONFIG,
-		},
-		{
-			Step: math.MaxInt32, //large, out-of-band value
-		},
+	len2StepStatusOob = []*idl.UpgradeStepStatus{
+		{Step: idl.UpgradeSteps_CONFIG},
+		{Step: math.MaxInt32}, //large, out-of-band value
 	}
 )
 
@@ -58,7 +42,7 @@ func TestStepStatuses_Len(t *testing.T) {
 		},
 		{
 			"length_2",
-			len2StepStatus_1lt2,
+			len2StepStatus1lt2,
 			2,
 		},
 	}
@@ -84,25 +68,25 @@ func TestStepStatuses_Less(t *testing.T) {
 	}{
 		{
 			"basic",
-			len2StepStatus_1lt2,
+			len2StepStatus1lt2,
 			args{0, 1},
 			true,
 		},
 		{
 			"reverse",
-			len2StepStatus_2lt1,
+			len2StepStatus2lt1,
 			args{0, 1},
 			false,
 		},
 		{
 			"out-of-band",
-			len2StepStatus_oob,
+			len2StepStatusOob,
 			args{1, 0},
 			true,
 		},
 		{
 			"out-of-band-reverse",
-			len2StepStatus_oob,
+			len2StepStatusOob,
 			args{0, 1},
 			false,
 		},
@@ -117,74 +101,22 @@ func TestStepStatuses_Less(t *testing.T) {
 }
 
 func TestStepStatuses_Swap(t *testing.T) {
-	type args struct {
-		i int
-		j int
-	}
-	tests := []struct {
-		name string
-		sIn  StepStatuses
-		sOut StepStatuses
-		args args
-	}{
-		{
-			"basic",
-			[]*idl.UpgradeStepStatus{
-				{
-					Step: idl.UpgradeSteps_UNKNOWN_STEP,
-				},
-				{
-					Step: idl.UpgradeSteps_CONFIG,
-				},
-			},
-			[]*idl.UpgradeStepStatus{
-				{
-					Step: idl.UpgradeSteps_CONFIG,
-				},
-				{
-					Step: idl.UpgradeSteps_UNKNOWN_STEP,
-				},
-			},
-			args{0, 1},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			tt.sIn.Swap(tt.args.i, tt.args.j)
-			for i := range tt.sIn {
-				if tt.sIn[i].GetStep() != tt.sOut[i].GetStep() {
-					t.Errorf("StepStatuses.Swap() for %v got %v, wanted %v", i, tt.sIn[i], tt.sOut[i])
-				}
-			}
-		})
+	stepStatuses := StepStatuses([]*idl.UpgradeStepStatus{
+		{Step: idl.UpgradeSteps_UNKNOWN_STEP},
+		{Step: idl.UpgradeSteps_CONFIG},
+	})
+	expected := StepStatuses([]*idl.UpgradeStepStatus{
+		{Step: idl.UpgradeSteps_CONFIG},
+		{Step: idl.UpgradeSteps_UNKNOWN_STEP},
+	})
+
+	stepStatuses.Swap(0, 1)
+	for i := range stepStatuses {
+		if stepStatuses[i].GetStep() != expected[i].GetStep() {
+			t.Errorf("StepStatuses.Swap() for %v got %v, wanted %v", i, stepStatuses[i], expected[i])
+		}
 	}
 }
-
-// XXX: do we really just want to be using well-formed data from gp_segment_configuration here?
-var (
-	len0PrimaryStatus = []*idl.PrimaryStatus{}
-	len1PrimaryStatus = []*idl.PrimaryStatus{
-		{
-			Dbid: 1,
-		},
-	}
-	len2PrimaryStatus_1lt2 = []*idl.PrimaryStatus{
-		{
-			Dbid: 1,
-		},
-		{
-			Dbid: 2,
-		},
-	}
-	len2PrimaryStatus_2lt1 = []*idl.PrimaryStatus{
-		{
-			Dbid: 2,
-		},
-		{
-			Dbid: 1,
-		},
-	}
-)
 
 func TestPrimaryStatuses_Len(t *testing.T) {
 	tests := []struct {
@@ -194,17 +126,17 @@ func TestPrimaryStatuses_Len(t *testing.T) {
 	}{
 		{
 			"empty",
-			len0PrimaryStatus,
+			[]*idl.PrimaryStatus{},
 			0,
 		},
 		{
 			"len1",
-			len1PrimaryStatus,
+			[]*idl.PrimaryStatus{{Dbid: 1}},
 			1,
 		},
 		{
 			"len2",
-			len2PrimaryStatus_1lt2,
+			[]*idl.PrimaryStatus{{Dbid: 1}, {Dbid: 2}},
 			2,
 		},
 	}
@@ -218,91 +150,24 @@ func TestPrimaryStatuses_Len(t *testing.T) {
 }
 
 func TestPrimaryStatuses_Less(t *testing.T) {
-	type args struct {
-		i int
-		j int
+	lessThan := PrimaryStatuses([]*idl.PrimaryStatus{{Dbid: 1}, {Dbid: 2}})
+	if got := lessThan.Less(0, 1); got != true {
+		t.Errorf("PrimaryStatuses.Less() = %v, want %v", got, true)
 	}
-	tests := []struct {
-		name string
-		s    PrimaryStatuses
-		args args
-		want bool
-	}{
-		{
-			"basic",
-			len2PrimaryStatus_1lt2,
-			args{0, 1},
-			true,
-		},
-		{
-			"reverse",
-			len2PrimaryStatus_2lt1,
-			args{0, 1},
-			false,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := tt.s.Less(tt.args.i, tt.args.j); got != tt.want {
-				t.Errorf("PrimaryStatuses.Less() = %v, want %v", got, tt.want)
-			}
-		})
+
+	greaterThan := PrimaryStatuses([]*idl.PrimaryStatus{{Dbid: 2}, {Dbid: 1}})
+	if got := greaterThan.Less(0, 1); got != false {
+		t.Errorf("PrimaryStatuses.Less() = %v, want %v", got, false)
 	}
 }
 
 func TestPrimaryStatuses_Swap(t *testing.T) {
-	type args struct {
-		i int
-		j int
-	}
-	tests := []struct {
-		name string
-		sIn  PrimaryStatuses
-		sOut PrimaryStatuses
-		args args
-	}{
-		{"basic",
-			[]*idl.PrimaryStatus{
-				{
-					Status:   idl.StepStatus_FAILED,
-					Dbid:     1,
-					Content:  3,
-					Hostname: "localhost",
-				},
-				{
-					Status:   idl.StepStatus_RUNNING,
-					Dbid:     2,
-					Content:  4,
-					Hostname: "localhost",
-				},
-			},
-			[]*idl.PrimaryStatus{
-				{
-					Status:   idl.StepStatus_RUNNING,
-					Dbid:     2,
-					Content:  4,
-					Hostname: "localhost",
-				},
-				{
-					Status:   idl.StepStatus_FAILED,
-					Dbid:     1,
-					Content:  3,
-					Hostname: "localhost",
-				},
-			},
-			args{0, 1},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			tt.sIn.Swap(tt.args.i, tt.args.j)
-			for i := range tt.sIn {
-				if tt.sIn[i].GetDbid() != tt.sOut[i].GetDbid() {
-					t.Errorf("PrimaryStatuses.Swap() index %v got %v expected %v", i, tt.sIn[i], tt.sOut[i])
-
-				}
-			}
-
-		})
+	primaryStatuses := PrimaryStatuses([]*idl.PrimaryStatus{{Dbid: 1}, {Dbid: 2}})
+	expected := []*idl.PrimaryStatus{{Dbid: 2}, {Dbid: 1}}
+	primaryStatuses.Swap(0, 1)
+	for i := range primaryStatuses {
+		if primaryStatuses[i].GetDbid() != expected[i].GetDbid() {
+			t.Errorf("PrimaryStatuses.Swap() index %v got %v expected %v", i, primaryStatuses[i], expected[i])
+		}
 	}
 }
